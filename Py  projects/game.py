f7042175d24207d967201e6cad9b296b53804de8
exvaibhav12 <vaibhavcnellikoppa@gmail.com>
2024-08-11 18:29:12 +0530

    Add files via upload

---
+++ Py  projects/game.py
@@ -0,0 +1,44 @@
+from thinker import *
+
+import  random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "GREEN"
+FOOD_COLOR = "RED"
+BACKGROUND_COLOR = "BLACK"
+
+
+class snake :
+
+    pass
+
+class food :
+
+    pass
+
+def next_turn() :
+
+    pass
+
+def change_direction( new direction ) :
+
+    pass
+
+def check_collections() :
+
+    pass
+
+def game_over() :
+
+    pass
+
+
+window = TK()
+window.title("snake game")
+window.resizable(False, False)
+
+window.mainloop()
